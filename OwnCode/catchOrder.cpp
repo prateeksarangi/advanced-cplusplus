a52a8273035766b24f10a2f4ce3e8b7383ee4d35
Prateek Sarangi <sarangiprateek80@gmail.com>
2020-09-19 13:12:37 +0530

    Ordering exception added.

---
+++ OwnCode/catchOrder.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <exception>
+using namespace std;
+
+void goesWrong() {
+    bool error1Detected = true;
+    bool error2Detected = false;
+
+    if (error1Detected) {
+        throw bad_alloc();
+    }
+
+    if (error2Detected) {
+        throw exception();
+    }
+}
+
+int main() {
+    try {
+        goesWrong();
+    }
+    catch (bad_alloc &e) {
+        cout<<"In bad alloc: "<<e.what()<<endl;
+    }
+    catch (exception &e) {
+        cout<<"In exception: "<<e.what()<<endl;
+    }
+}
